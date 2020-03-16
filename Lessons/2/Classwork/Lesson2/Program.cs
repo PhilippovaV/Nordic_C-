93a51169ffb1ab4a6cdd5353fb334e760a5ca459
Valerie <Philxo.LO@gmail.com>
2020-03-16 22:09:57 +0300

    ops

---
+++ Lessons/2/Classwork/Lesson2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lesson2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int a = 500000000;
+			short b = 32765;
+			float c = 56.5f;
+			decimal d = 1.345674737m;
+			Console.WriteLine(a + b);
+					
+		
+
+
+		}
+	}
+}
